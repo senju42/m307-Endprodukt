fa3e4938a0516cd15d7ef0ac36b837e84a2f7ed7
senju42 <142969130+senju42@users.noreply.github.com>
2023-11-22 09:37:40 +0100

    upload

---
+++ erfasst.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kunde erfasst</title>
+</head>
+<body>
+    <a href="index.php">Zurück</a><br>
+    <a href="kundenerfassung.html">weiteren Kunde erfassen</a>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kundenliste</title>
+</head>
+<body>
+    <a href="kundenerfassung.html">Kunde erfassen</a>
+</body>
+</html>
