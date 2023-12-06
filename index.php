bdc4617b7d6b22424ec669fa6cebd2c4d30dc2e6
senju42 <142969130+senju42@users.noreply.github.com>
2023-12-06 08:57:09 +0100

    index db besucher

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="de">
-<head> 
+
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kundenliste</title>
@@ -8,37 +9,55 @@
         body {
             font-family: Arial, sans-serif;
         }
+
         table {
             border-collapse: collapse;
             width: 100%;
             margin-top: 20px;
         }
-        th, td {
+
+        th,
+        td {
             border: 1px solid #dddddd;
             text-align: left;
             padding: 8px;
         }
+
         th {
             background-color: #C5DF9A;
         }
+
         a {
-    text-decoration: none;
-    padding: 10px;
-    background-color: #8CBF35;
-    color: #fff;
-    border-radius: 5px;
-    margin-top: 20px; 
-    display: inline-block; 
-}
+            text-decoration: none;
+            padding: 10px;
+            background-color: #8CBF35;
+            color: #fff;
+            border-radius: 5px;
+            margin-top: 20px;
+            display: inline-block;
+        }
     </style>
 </head>
-<body>
-<h1>Erfasste Kunden</h1>
 
-<?php
-        $kunden = array(
-            array("Mannhart", "Manfred", "Fidlistrasse 48", "ST.Gallen", "9000", "ST.Gallen", "manfred.mannhart@gmail.com", "079 133 80 90", "Einzelperson", "Aktiv"),
-        );
+<body>
+    <h1>Erfasste Kunden</h1>
+
+    <?php
+    $kunden = array(
+        array("Mannhart", "Manfred", "Fidlistrasse 48", "ST.Gallen", "9000", "ST.Gallen", "manfred.mannhart@gmail.com", "079 133 80 90", "Einzelperson", "Aktiv"),
+    );
+
+    //VERBINDUNG DB
+    // User und Passwort
+    $dbuser = 'Besucher';
+    $dbpass = '1-4m-h3r3t0r34d';
+
+    //Verbindung erstellen
+    try {
+        $db = new \PDO('mysql:host=localhost;dbname=m290_endprodukt_kunden-db', $dbuser, $dbpass);
+    } catch (\PDOException $e) {
+        die("Verbindung zur Datenbank fehlgeschlagen: " . $e->getMessage());
+    }
     ?>
 
     <table>
@@ -55,15 +74,16 @@
             <th>Status</th>
         </tr>
         <?php
-            foreach ($kunden as $kunde) {
-                echo "<tr>";
-                foreach ($kunde as $info) {
-                    echo "<td>$info</td>";
-                }
-                echo "</tr>";
+        foreach ($kunden as $kunde) {
+            echo "<tr>";
+            foreach ($kunde as $info) {
+                echo "<td>$info</td>";
             }
+            echo "</tr>";
+        }
         ?>
     </table>
 </body>
 <a href="kundenerfassung.php">Kunde erfassen</a>
+
 </html>
