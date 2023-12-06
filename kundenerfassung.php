9d1e4edc7b785c56fb22b1ce1c51b1e8bd09c99b
senju42 <142969130+senju42@users.noreply.github.com>
2023-12-06 08:45:49 +0100

    verbindung db 1

--- kundenerfassung.php
+++ kundenerfassung.php
@@ -59,92 +59,74 @@ $kundenklasse = $_POST['kundenklasse'] ?? "";
 
 // VERBINDUNG MIT DB
 // User und Passwort
-$dbuser = '';
+$dbuser = 'root';
 $dbpass = '';
 
 //Verbindung erstellen
 try {
-        $db = new \PDO('mysql:host=localhost;dbname=m290_')
+        $db = new \PDO('mysql:host=localhost;dbname=m290_endprodukt_kunden-db', $dbuser, $dbpass);
+} catch (\PDOException $e) {
+        die("Verbindung zur Datenbank fehlgeschlagen: " . $e->getMessage());
 }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="de">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="style.css" />
-    <title>Kundenerfassung</title>
-  </head>
-  <body>
-    <h1>Kundenerfassung</h1>
-    <form method="POST" action="erfasst.php">
-      <label for="vorname">Vorname</label>
-      <input type="vorname" name="vorname" id="vorname" />
 
-      <label for="nachname">Nachname</label>
-      <input type="nachname" name="nachname" id="nachname" />
+<head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="style.css" />
+        <title>Kundenerfassung</title>
+</head>
 
-      <label for="strasse">Strasse & Nr.</label>
-      <input type="strasse" name="strasse" id="strasse" />
+<body>
+        <h1>Kundenerfassung</h1>
+        <form method="POST" action="erfasst.php">
+                <label for="vorname">Vorname</label>
+                <input type="vorname" name="vorname" id="vorname" />
 
-      <label for="ort">Ortsname</label>
-      <input type="ort" name="ort" id="ort" />
+                <label for="nachname">Nachname</label>
+                <input type="nachname" name="nachname" id="nachname" />
 
-      <label for="plz">Postleitzahl</label>
-      <input type="plz" name="plz" id="plz" />
+                <label for="strasse">Strasse & Nr.</label>
+                <input type="strasse" name="strasse" id="strasse" />
 
-      <label for="kanton">Kanton</label>
-      <input type="kanton" name="kanton" id="kanton" />
+                <label for="ort">Ortsname</label>
+                <input type="ort" name="ort" id="ort" />
 
-      <label for="email">E-Mail</label>
-      <input type="email" name="email" id="email" />
+                <label for="plz">Postleitzahl</label>
+                <input type="plz" name="plz" id="plz" />
 
-      <label for="telefon">Telefonnummer</label>
-      <input type="telefon" name="telefon" id="telefon" />
-      <div>
-        <p>Kundenklasse</p>
-        <input
-          type="radio"
-          name="kundenklasse"
-          id="firmenkunde"
-          value="firmenkunde"
-        />
-        <label for="firmenkunde"><i>Firmenkunde</i></label
-        ><br />
-        
-        <input
-          type="radio"
-          name="kundenklasse"
-          id="einzelperson"
-          value="einzelperson"
-        />
-        <label for="einzelperson"><i>Einzelperson</i></label
-        ><br />
-        
-        <input
-          type="radio"
-          name="kundenklasse"
-          id="partner"
-          value="partner"
-        />
-        <label for="partner"><i>Partner</i></label
-        ><br />
-        
-        <input
-          type="radio"
-          name="kundenklasse"
-          id="bljs"
-          value="bljs"
-        />
-        <label for="bljs"><i>Betriebslehrjahrstelle</i></label
-        ><br />
-      </div>
-      <div class="container">
-      <input type="submit" value="Speichern">      </div>
-    </form>
-  </body>
+                <label for="kanton">Kanton</label>
+                <input type="kanton" name="kanton" id="kanton" />
+
+                <label for="email">E-Mail</label>
+                <input type="email" name="email" id="email" />
+
+                <label for="telefon">Telefonnummer</label>
+                <input type="telefon" name="telefon" id="telefon" />
+                <div>
+                        <p>Kundenklasse</p>
+                        <input type="radio" name="kundenklasse" id="firmenkunde" value="firmenkunde" />
+                        <label for="firmenkunde"><i>Firmenkunde</i></label><br />
+
+                        <input type="radio" name="kundenklasse" id="einzelperson" value="einzelperson" />
+                        <label for="einzelperson"><i>Einzelperson</i></label><br />
+
+                        <input type="radio" name="kundenklasse" id="partner" value="partner" />
+                        <label for="partner"><i>Partner</i></label><br />
+
+                        <input type="radio" name="kundenklasse" id="bljs" value="bljs" />
+                        <label for="bljs"><i>Betriebslehrjahrstelle</i></label><br />
+                </div>
+                <div class="container">
+                        <input type="submit" value="Speichern">
+                </div>
+        </form>
+</body>
+
 </html>
 
 <!-- OLD CODE --
