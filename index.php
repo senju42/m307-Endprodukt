993364992a971cfaa6e7c759360436f80b31e065
NBI25 <nils.bingesser@sbw-media.ch>
2023-12-06 09:03:49 +0100

    kommentare

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html lang="de">
 <head> 
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kundenliste</title>
+   
+<!-- Deklaration des HTML-Dokumenttyps und der Sprache -->
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Kundenliste</title> 
     <style>
+        
+        /* Styling für das Aussehen der Seite */
         body {
-            font-family: Arial, sans-serif;
+            font-family: Arial, sans-serif; 
         }
         table {
             border-collapse: collapse;
@@ -22,26 +26,32 @@
             background-color: #C5DF9A;
         }
         a {
-    text-decoration: none;
-    padding: 10px;
-    background-color: #8CBF35;
-    color: #fff;
-    border-radius: 5px;
-    margin-top: 20px; 
-    display: inline-block; 
-}
+           
+            text-decoration: none;
+            padding: 10px;
+            background-color: #8CBF35;
+            color: #fff;
+            border-radius: 5px;
+            margin-top: 20px; 
+            display: inline-block; 
+        }
     </style>
 </head>
 <body>
 <h1>Erfasste Kunden</h1>
 
+
 <?php
-        $kunden = array(
-            array("Mannhart", "Manfred", "Fidlistrasse 48", "ST.Gallen", "9000", "ST.Gallen", "manfred.mannhart@gmail.com", "079 133 80 90", "Einzelperson", "Aktiv"),
-        );
-    ?>
+    // PHP-Code für die Kundenliste
+    
+    $kunden = array(
+        array("Mannhart", "Manfred", "Fidlistrasse 48", "ST.Gallen", "9000", "ST.Gallen", "manfred.mannhart@gmail.com", "079 133 80 90", "Einzelperson", "Aktiv"),
+    );
+?>
+
 
     <table>
+        <!-- Tabelle zur Anzeige der Kundeninformationen -->
         <tr>
             <th>Name</th>
             <th>Vorname</th>
@@ -58,12 +68,12 @@
             foreach ($kunden as $kunde) {
                 echo "<tr>";
                 foreach ($kunde as $info) {
-                    echo "<td>$info</td>";
+                    echo "<td>$info</td>"; 
                 }
                 echo "</tr>";
             }
         ?>
     </table>
 </body>
-<a href="kundenerfassung.php">Kunde erfassen</a>
+<a href="kundenerfassung.php">Kunde erfassen</a> <!-- Link zur Seite für die Erfassung neuer Kunden -->
 </html>
