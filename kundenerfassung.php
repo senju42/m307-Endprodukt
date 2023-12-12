a9a83c7a49b0d6ded22decfd41a0930ea71cf049
senju42 <142969130+senju42@users.noreply.github.com>
2023-12-12 16:10:43 +0100

    gpt 3.0

--- kundenerfassung.php
+++ kundenerfassung.php
@@ -3,6 +3,7 @@
 $vorname = $_POST['vorname'] ?? "";
 $nachname = $_POST['nachname'] ?? "";
 $strasse = $_POST['strasse'] ?? "";
+$status = $_POST['status'] ?? "";
 $ort = $_POST['ort'] ?? "";
 $kontakt = $_POST['kontakt'] ?? "";
 $kundenklasse = $_POST['kundenklasse'] ?? "";
@@ -14,65 +15,68 @@ $dbpass = '';
 
 //Verbindung erstellen
 try {
-        $db = new \PDO('mysql:host=localhost;dbname=m290_endprodukt_kunden-db', $dbuser, $dbpass);
+    $db = new \PDO('mysql:host=localhost;dbname=m290_endprodukt_kunden-db', $dbuser, $dbpass);
 } catch (\PDOException $e) {
-        die("Verbindung zur Datenbank fehlgeschlagen: " . $e->getMessage());
+    die("Verbindung zur Datenbank fehlgeschlagen: " . $e->getMessage());
 }
 
 // SQL-Statement für Kunden
 $sqlKunden = "
-INSERT INTO `kunden` (`Kunden_ID`, `Vorname`, `Nachname`, `Strasse&Nummer`, `status`, `Kontakt_Kontakt_ID`, `Wohnort_Wohnort_ID`, `Klasse_Klasse_ID`) 
-             VALUES         (NULL,  :Vorname,  :Nachname,         :Strasse,  :Status,             :Kontakt,                 :Ort,            :Klasse);";
+    INSERT INTO `kunden` (`Kunden_ID`, `Vorname`, `Nachname`, `Strasse&Nummer`, `status`, `Kontakt_Kontakt_ID`, `Wohnort_Wohnort_ID`, `Klasse_Klasse_ID`) 
+    VALUES (NULL, :Vorname, :Nachname, :Strasse, :Status, :Kontakt, :Ort, :Klasse);
+";
 
 // SQL-Statement für Klasse
 $sqlKlasse = "
-INSERT INTO `klasse` (`Klasse_ID`, `Klasse_Name`)
-             VALUES         (NULL,  :Klasse_Name);";
+    INSERT INTO `klasse` (`Klasse_ID`, `Klasse_Name`)
+    VALUES (NULL, :Klasse_Name);
+";
 
 // SQL-Statement für Kontakt
 $sqlKontakt = "
-INSERT INTO `kontakt`(`Kontakt_ID`, `Email`, `Telefon`)
-             VALUES          (NULL,  :Email,  :Telefon);";
+    INSERT INTO `kontakt`(`Kontakt_ID`, `Email`, `Telefon`)
+    VALUES (NULL, :Email, :Telefon);
+";
 
 // SQL-Statement für Wohnort
 $sqlWohnort = "
-INSERT INTO `wohnort`(`Wohnort_ID`, `Ortsname`, `Postleitzahl`, `Kanton`)
-             VALUES          (NULL,  :Ortsname,  :Postleitzahl,  :Kanton);";
+    INSERT INTO `wohnort`(`Wohnort_ID`, `Ortsname`, `Postleitzahl`, `Kanton`)
+    VALUES (NULL, :Ortsname, :Postleitzahl, :Kanton);
+";
 
 //Abfrage abschicken
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $stmt = $db->prepare($sql);
+    // Kunden-Daten einfügen
+    $stmtKunden = $db->prepare($sqlKunden);
+    $stmtKunden->bindParam(':Vorname', $vorname);
+    $stmtKunden->bindParam(':Nachname', $nachname);
+    $stmtKunden->bindParam(':Strasse', $strasse);
+    $stmtKunden->bindParam(':Status', $status);
+    $stmtKunden->bindParam(':Kontakt', $kontakt);
+    $stmtKunden->bindParam(':Ort', $ort);
+    $stmtKunden->bindParam(':Klasse', $kundenklasse);
+    $stmtKunden->execute();
 
-        // Kunden-Daten einfügen
-        $stmtKunden = $db->prepare($sqlKunden);
-        $stmtKunden->bindParam(':Vorname', $vorname);
-        $stmtKunden->bindParam(':Nachname', $nachname);
-        $stmtKunden->bindParam(':Strasse', $strasse);
-        $stmtKunden->bindParam(':Status', $status); // Achtung: Du musst $status definieren!
-        $stmtKunden->bindParam(':Kontakt', $kontakt);
-        $stmtKunden->bindParam(':Ort', $ort);
-        $stmtKunden->bindParam(':Klasse', $kundenklasse);
-        $stmtKunden->execute();
+    // Klasse-Daten einfügen
+    $stmtKlasse = $db->prepare($sqlKlasse);
+    $stmtKlasse->bindParam(':Klasse_Name', $kundenklasse);
+    $stmtKlasse->execute();
 
-        // Klasse-Daten einfügen
-        $stmtKlasse = $db->prepare($sqlKlasse);
-        $stmtKlasse->bindParam(':Klasse_Name', $kundenklasse);
-        $stmtKlasse->execute();
+    // Kontakt-Daten einfügen
+    $stmtKontakt = $db->prepare($sqlKontakt);
+    $stmtKontakt->bindParam(':Email', $kontakt);
+    $stmtKontakt->bindParam(':Telefon', ''); // Kein Feld im Formular
+    $stmtKontakt->execute();
 
-        // Kontakt-Daten einfügen
-        $stmtKontakt = $db->prepare($sqlKontakt);
-        $stmtKontakt->bindParam(':Email', $kontaktEmail); // Achtung: Du musst $kontaktEmail definieren!
-        $stmtKontakt->bindParam(':Telefon', $kontaktTelefon); // Achtung: Du musst $kontaktTelefon definieren!
-        $stmtKontakt->execute();
-
-        // Wohnort-Daten einfügen
-        $stmtWohnort = $db->prepare($sqlWohnort);
-        $stmtWohnort->bindParam(':Ortsname', $wohnortOrtsname); // Achtung: Du musst $wohnortOrtsname definieren!
-        $stmtWohnort->bindParam(':Postleitzahl', $wohnortPLZ); // Achtung: Du musst $wohnortPLZ definieren!
-        $stmtWohnort->bindParam(':Kanton', $wohnortKanton); // Achtung: Du musst $wohnortKanton definieren!
-        $stmtWohnort->execute();
+    // Wohnort-Daten einfügen
+    $stmtWohnort = $db->prepare($sqlWohnort);
+    $stmtWohnort->bindParam(':Ortsname', $ort);
+    $stmtWohnort->bindParam(':Postleitzahl', ''); // Kein Feld im Formular
+    $stmtWohnort->bindParam(':Kanton', ''); // Kein Feld im Formular
+    $stmtWohnort->execute();
 }
 ?>
+
 
 <!DOCTYPE html>
 <html lang="de">
@@ -153,76 +157,3 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 </body>
 
 </html>
-
-<!-- OLD CODE --
-
-<!DOCTYPE html>
-<html lang="de">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Kundenerfassung</title>
-  </head>
-  <body>
-    <h1>Kundenerfassung</h1>
-    <form method="POST" action="erfasst.php"">
-      <label for="vorname">Vorname</label><br />
-      <input type="vorname" name="vorname" id="vorname" /><br />
-
-      <label for="nachname">Nachname</label><br />
-      <input type="nachname" name="nachname" id="nachname" /><br />
-
-      <label for="strasse">Strasse & Nr.</label><br />
-      <input type="strasse" name="strasse" id="strasse" /><br />
-
-      <label for="ort">Ortsname</label><br />
-      <input type="ort" name="ort" id="ort" /><br />
-
-      <label for="plz">Postleitzahl</label><br />
-      <input type="plz" name="plz" id="plz" /><br />
-
-      <label for="kanton">Kanton</label><br />
-      <input type="kanton" name="kanton" id="kanton" /><br />
-
-      <label for="email">E-Mail</label><br />
-      <input type="email" name="email" id="email" /><br />
-
-      <label for="telefon">Telefonnummer</label><br />
-      <input type="telefon" name="telefon" id="telefon" /><br />
-
-      <p>Kundenklasse</p>
-      <input
-        type="radio"
-        name="kundenklasse"
-        id="firmenkunde"
-        value="firmenkunde"
-      />
-      <label for="firmenkunde"><i>Firmenkunde</i></label><br />
-      
-      <input
-        type="radio"
-        name="kundenklasse"
-        id="einzelperson"
-        value="einzelperson"
-        />
-      <label for="einzelperson"><i>Einzelperson</i></label><br />
-      
-      <input 
-        type="radio" 
-        name="kundenklasse" 
-        id="partner" 
-        value="partner" 
-        />
-      <label for="partner"><i>Partner</i></label><br />
-      
-      <input 
-        type="radio" 
-        name="kundenklasse" 
-        id="bljs" 
-        value="bljs" 
-        />
-        <label for="bljs"><i>Betriebslehrjahrstelle</i></label>
-        <input type="submit" value="Speichern">
-      </form>
-  </body>
-</html>-->
