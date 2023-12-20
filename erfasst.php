06c1666fef9082faac84a87eaf6a133043e4ab73
Veneruso <Diego.Veneruso@sbw-media.ch>
2023-12-20 09:01:55 +0100

    Final

--- erfasst.php
+++ erfasst.php
@@ -1,6 +1,22 @@
 <?php
 $id = $_GET['id'];
+
+$dbuser = 'root';
+$dbpass = '';
+
+try {
+    $db = new PDO('mysql:host=localhost;dbname=m290_endprodukt_kunden-db', $dbuser, $dbpass);
+} catch (PDOException $e) {
+    die("Verbindung zur Datenbank fehlgeschlagen: " . $e->getMessage());
+}
+
+$sql = "SELECT * FROM `kunden_klasse_kontakt_wohnort` WHERE `Kunden_ID` = :id";
+$stmt = $db->prepare($sql);
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+$stmt->execute();
+$kunde = $stmt->fetch(PDO::FETCH_ASSOC);
 ?>
+
 <!DOCTYPE html>
 <html lang="de">
 <head>
@@ -11,12 +27,19 @@ $id = $_GET['id'];
 <body>
     <h1>Kunde erfolgreich erfasst</h1>
     <p>Folgende Daten wurden gespeichert:</p>
-    <ul>
-        <?php
-echo $id; 
-$sql = "SELECT *  FROM `kunden` WHERE `Kunden_ID` = $id;";
-        ?>
-    </ul>
+    <?php if ($kunde): ?>
+        <ul>
+            <li>ID: <?php echo htmlspecialchars($kunde['Kunden_ID']); ?></li>
+            <li>Vorname: <?php echo htmlspecialchars($kunde['Vorname']); ?></li>
+            <li>Nachname: <?php echo htmlspecialchars($kunde['Nachname']); ?></li>
+            <li>Strasse & Hausnummer: <?php echo htmlspecialchars($kunde['Strasse&Nummer']); ?></li>
+            <li>Status: <?php echo htmlspecialchars($kunde['status']); ?></li>
+            <li>Kundenklasse: <?php echo htmlspecialchars($kunde['Klasse_Klasse_ID']); ?></li>
+            <!-- Fügen Sie hier weitere Felder hinzu, die Sie anzeigen möchten -->
+        </ul>
+    <?php else: ?>
+        <p>Keine Daten gefunden.</p>
+    <?php endif; ?>
     <a href="index.php">Zurück zur Kundenliste</a><br>
     <a href="kundenerfassung.php">Weiteren Kunden erfassen</a>
 </body>
